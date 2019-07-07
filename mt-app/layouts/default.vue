598a78c5fa31b3719a97fa1eb6a651ead36cc89e
LinkChenzy <linkchenzy@gmail.com>
2019-07-07 11:27:02 +0800

    refactor: default layout complete 除了中间列表需接口才能完成

--- mt-app/layouts/default.vue
+++ mt-app/layouts/default.vue
@@ -2,15 +2,18 @@
   <el-container>
     <el-header><my-header /></el-header>
     <el-main><nuxt /></el-main>
-    <el-footer>Footer</el-footer>
+    <el-footer><my-footer /></el-footer>
   </el-container>
 </template>
 
 <script>
 import MyHeader from '@/components/public/header/index'
+import MyFooter from '@/components/public/footer/index'
+
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 }
 </script>
