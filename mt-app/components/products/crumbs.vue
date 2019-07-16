bff54fcd4b062c3cb6caf84e1b8624ccd57fac22
LinkChenzy <linkchenzy@gmail.com>
2019-07-16 13:06:39 +0800

    feat: product page components

---
+++ mt-app/components/products/crumbs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-crumbs">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">美团</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ decodeURIComponent(keyword) }}</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    keyword: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ mt-app/pages/products.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-row class="page-product">
+    <el-col :span="19">
+      <!-- <crumbs /> -->
+      <!-- <categroy /> -->
+      <!-- <list /> -->
+    </el-col>
+    <el-col :span="5">
+      2
+    </el-col>
+  </el-row>
+</template>
+<script>
+// import Crumbs from '@/components/products/crumbs.vue'
+// import Categroy from '@/components/products/categroy.vue'
+// import List from '@/components/products/list.vue'
+export default {
+  components: {
+    // Crumbs,
+    // Categroy
+    // List
+  }
+}
+</script>
+<style lang="scss"></style>
